27f0745dcaf8ab0b9ef4be3f8b905093c8147707
tommyb841 <thomasb841@yahoo.com>
2020-10-13 16:44:38 -0400

    update

--- MoreExpress/skategrounds/app.js
+++ MoreExpress/skategrounds/app.js
@@ -1,7 +1,11 @@
+//gets express dependancy
 var express = require('express');
+//Mysql dependancy
 var mysql = require('mysql');
 
+// creates a call to express
 const app = express();
+//creates a varible that will allow login/connection to a database.
 const db = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
@@ -9,10 +13,12 @@ const db = mysql.createConnection({
 	database: 'Inventory_app'
 });
 
-
+//not sure what this does
 var bodyParser = require("body-parser");
+//not sure what this does
 const axios = require('axios');
 
+//connection to database
 db.connect((err) => {
 	if(err) {
 		throw err;
@@ -20,9 +26,13 @@ db.connect((err) => {
 		console.log('connected');
 	}
 });
+
+//notsure what this does
 app.use(bodyParser.urlencoded({extended: true}));
+//use to read ejs pages??
 app.set("view engine", "ejs");
 
+//list of elements that will be displayed to page
 var skategrounds = [
 		{name: "Brooklyn banks", 
 			image:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.qhoIwYVgKBkLLIH8Ia_wogHaFk%26pid%3DApi&f=1"},
@@ -32,17 +42,19 @@ var skategrounds = [
 			image:"https://images.pexels.com/photos/208644/pexels-photo-208644.jpeg?auto=compress&cs=tinysrgb&h=350"}
 	]; 
 
+//landing/home page
 app.get("/",(req,res) => {
 	res.render("landing");
 });
 
+//skaterounds pages displays the skate grounds
 app.get("/skategrounds",(req,res) => {
 	res.render("skategrounds",{skategrounds: skategrounds});
 });
 
 
 app.post("/skategrounds", function(req,res){
-	//get data from form and add to skategrounds arrayi
+	//get data from form and add to skategrounds array
 	var name = req.body.name;
 	var image = req.body.image
 	var newSkateground = {name: name, image: image};
@@ -58,12 +70,21 @@ app.get("/skategrounds/new",(req,res) => {
 
 //######################################
 //this is trial database code
+
 app.get("/skategrounds/items",function(req,res){
-	db.query("SELECT * FROM Items", function(err, result, rows){
-		if(err) throw err;
-		console.log("result: " + rows);
-	});
+	res.render("items");
 });
+	db.query("SELECT * FROM Items",
+		function(err, result, rows, fields){
+			if(err) {throw err;
+			console.log("result: " + result);
+			return;
+		}
+	
+			rows.forEach((result, fields)=>{
+			console.log(result); 
+			})
+		});
 //#####################################
 app.listen(process.env.PORT, process.env.IP, function(){
 		console.log("Skategrounds App has started!!!");
