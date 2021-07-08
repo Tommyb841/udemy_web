87f9901ce0989a7ff28d6325b3ff84efddbe0ffd
tommyb841 <thomasb841@yahoo.com>
2021-07-08 17:02:59 -0400

    updated index with routes, nothing added to edit page

--- MoreExpress/skategrounds/index.js
+++ MoreExpress/skategrounds/index.js
@@ -9,6 +9,8 @@ const Skateground = require('./models/skategrounds');
 const catchAsync = require('./utilities/catchAsync');
 const ExpressError = require('./utilities/ExpressError'); 
 const Review = require('./models/reviews');
+const skategrounds = require('./routes/skategrounds');
+
 //Mongodb connection
 mongoose.connect('mongodb://localhost:27017/skategrounds', { 
 	useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
@@ -29,92 +31,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(methodOverride("_method"))
 app.use(express.urlencoded({ extended: true}))
 
-const validateSkateground = (req,res, next) => {
-	const { error } = skategroundSchema.validate(req.body);
-	if (error) {
-		const msg = error.details.map(el => el.message).join(',')
-		throw new ExpressError(msg,404)
-	} else {
-		next();
-	}
-}
-const validateReview = (req, res, next) => {
-	const {error} = reviewSchema.validate(req.body)
-	if (error) {
-		const msg = error.details.map(el => el.message).join(',')
-		throw new ExpressError(msg,404)
-	} else {
-		next();
-	}
-}
+
+
+app.use('/skategrounds', skategrounds)
+
 //App get, post, delete, put functions
 app.get('/', (req,res) => {
 	res.render('landing')
 })
-
-app.get('/skategrounds', catchAsync( async (req,res) => {
-	const spots = await Skateground.find({});
-	res.render('skategrounds/index', { spots })
-}))
-	
-app.get('/skategrounds/new', catchAsync( async (req,res) => {
-	res.render('skategrounds/new')
-}))
-
-app.get('/skategrounds/:id', catchAsync( async (req,res) => {
-	const { id } = req.params;
-	const spot = await Skateground.findById(req.params.id).populate('reviews')
-	res.render('skategrounds/show', {spot})
-}))
-
-app.post('/skategrounds',validateSkateground, catchAsync( async (req, res, next) => {
-//		if(!req.body.skateground) throw new ExpressError('Invalid skateground data', 400);
-    const skateground = new Skateground(req.body.skateground);
-    await skateground.save();
-    res.redirect(`/skategrounds/${skateground._id}`)
-}))
-
-app.delete('/skategrounds/:id', catchAsync( async (req,res) => {
-		const { id } = req.params;
-		const del = await Skateground.findByIdAndDelete(id);
-		res.redirect('/skategrounds')
-
-}))
-app.get('/skategrounds/:id/edit', catchAsync( async(req,res) => {
-	const { id } = req.params;
-	const spot = await Skateground.findById(id)
-	res.render('skategrounds/edit', {spot})
-}))
-
-
-app.get('/errPage', (err, req, res) => {
-	if (!err.message) err.message = 'Oh No, Something Went Wrong!'
-  res.render('errPage', {err});
-	console.log('got this page lol ')
-})   
-
-app.post('/skategrounds/:id/reviews',validateReview,  catchAsync(async (req,res) =>{
-	const spot = await Skateground.findById(req.params.id);
-	const review = new Review(req.body.review);
-	spot.reviews.push(review);
-	await review.save();
-	await spot.save();
-	res.redirect(`/skategrounds/${spot._id}`);
-}))
-
-app.delete('/skategrounds/:id/reviews/:reviewId', catchAsync( async (req,res) =>{
-	const {id, reviewId} = req.params;
-	await Skateground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-	await Review.findByIdAndDelete(reviewId);
-	res.redirect(`/skategrounds/${id}`)
-}))
-
-
-app.put('/skategrounds/:id', validateSkateground, catchAsync( async(req,res) => {
-	const { id } = req.params;
-	const spot = await Skateground.findByIdAndUpdate(id, req.body.skateground, {runValidators: true, new: true});
-	res.redirect(`/skategrounds/${spot._id}`);
-}))
 
 app.all('*', (req, res, next) => {
   next(new ExpressError('Page Not Found', 404))
@@ -126,6 +50,11 @@ app.use(( err, req, res, next) => {
   res.status(statusCode).render('errPage', {err});
 })
 
+app.get('/errPage', (err, req, res) => {
+	if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+  res.render('errPage', {err});
+	console.log('got this page lol ')
+}) 
 
 app.listen(3000, () => {
 	console.log("app is listening on port 3000!")
