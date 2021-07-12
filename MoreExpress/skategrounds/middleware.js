d810130b4172161fc22d22fdd7682a57dd770ec8
tommyb841 <thomasb841@gmail.com>
2021-07-12 18:21:29 -0400

    added validations to middleware

--- MoreExpress/skategrounds/middleware.js
+++ MoreExpress/skategrounds/middleware.js
@@ -1,3 +1,11 @@
+//middleware requirements
+const ExpressError = require('./utilities/ExpressError'); 
+const { skategroundSchema, reviewSchema } = require('./schemas.js')
+const Skateground = require('./models/skategrounds'); 
+const catchAsync = require('./utilities/catchAsync');
+const Review = require('./models/reviews');
+
+//isLoggedIn module
 module.exports.isLoggedIn = (req, res, next) => {
 	console.log("user: ", req.user);
 	if (!req.isAuthenticated()) {
@@ -7,3 +15,50 @@ module.exports.isLoggedIn = (req, res, next) => {
 	}
 	next();
 }
+
+//skateground validation 
+module.exports.validateSkateground = (req, res, next) => {
+	const { error } = skategroundSchema.validate(req.body);
+	if (error) {
+		const msg = error.details.map(el => el.message).join(',')
+		throw new ExpressError(msg,404)
+	} else {
+		next();
+	}
+}
+
+//isAuthor module
+module.exports.isAuthor = async (req, res, next) =>{
+	const { id } = req.params;
+	const skateground = await	Skateground.findById(id);
+	console.log("THis is the isAuthor middleware" , req.params )
+	if (!skateground.author.equals(req.user._id)) {
+		req.flash('error', 'You do not have permission to do that!!');
+		return res.redirect(`/skategrounds/${id}`);
+	}
+ 	next();
+}
+	
+
+// isReviewAuthor module
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if (!review.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect(`/skategrounds/${id}`);
+    }
+    next();
+}
+
+//validateReview module
+module.exports.validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
