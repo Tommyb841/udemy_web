c98442d8e43c18f802a851d164f3bff06629ed9f
tommyb841 <thomasb841@gmail.com>
2022-01-27 18:30:52 -0500

    added spare code and todo list

---
+++ MoreExpress/skategrounds/spareCode.js
@@ -0,0 +1,17 @@
+
+<!-- 
+	add to the form a loop through the atrributes list, 
+	display all optional atrributes to the park.
+	each one will be connected with a check box.
+	if the checkbox is selected/true it will need to be added to the proper attribute in the database with the form submissiom.
+
+	<div id="attributes">
+							<p> this is where the atrributes go</p>
+							<%for(let x in attributes) {%>
+							<div class="form-check form-switch">
+								<input class="form-check-input" type="checkbox"  name="skateground[attributes]" id="attributes" default>
+								<label class="form-check-label" for="attributes"><%=attributes[x] %></label>
+							</div>
+							<%} %>
+					</div>  
+-->
