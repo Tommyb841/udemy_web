d9ef0b19104b2c02ec103a47a9a841dc0a3f2995
tommyb841 <thomasb841@gmail.com>
2022-01-27 18:31:15 -0500

    added attribute schema

--- MoreExpress/skategrounds/schemas.js
+++ MoreExpress/skategrounds/schemas.js
@@ -3,10 +3,17 @@ const Joi = require('joi');
 module.exports.skategroundSchema = Joi.object({
 		skateground: Joi.object({  
 		title: Joi.string().min(1).max(40).required(),
-		image: Joi.string().required(),
+	//	image: Joi.string().required(),
 		description: Joi.string().min(5).max(200).required(),
 	}).required()
 });
+
+module.exports.attributeSchema = Joi.object({
+	attribute: Joi.object({
+		body: Joi.string().required(),
+		description: Joi.string().min(1).max(200).required()
+	}).required()
+})
 
 module.exports.reviewSchema = Joi.object({
 	review: Joi.object({
